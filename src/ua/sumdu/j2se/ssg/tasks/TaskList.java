e55810d47a0681c621e2f644ee00b0ac76c9da95
sergiosssg <sergiosssg@gmail.com>
2018-02-20 16:15:46 +0200

    Stage 5 --- sub step 2nd      added new class Tasks with static methods :      - incomming(Iterable<Task>, Date, Date );      - calendar(Iterable<Task>, Date, Date)

--- src/ua/sumdu/j2se/ssg/tasks/TaskList.java
+++ src/ua/sumdu/j2se/ssg/tasks/TaskList.java
@@ -1,7 +1,5 @@
 package ua.sumdu.j2se.ssg.tasks;
 
-import java.util.Date;
-
 /**
  * Created by SSG on 13.06.2017.
  */
@@ -22,53 +20,4 @@ public abstract class TaskList implements Iterable<Task>{
     } // isEmpty()
 
     abstract public Task getTask(int index);
-
-    public TaskList incoming(int from, int to){
-        TaskList returnedTaskArray =  new ArrayTaskList();
-
-        try {
-            if(from < 0 || to < 0) throw new IllegalArgumentException("Arguments can't be negative");
-
-            Date fromDate = new Date(from);
-            Date toDate = new Date(to);
-
-            if(fromDate.after(toDate)) throw new IllegalArgumentException("Difference between second and first arguments can't be negative");
-
-            if( !isEmpty()){
-                for(int index_Of_Task = 0; index_Of_Task < size(); index_Of_Task++){
-                    Task tt = getTask(index_Of_Task);
-                    if(!tt.isActive() || (tt.getStartTime().after(toDate) || !tt.getEndTime().after(fromDate) )){
-                        // go to the next task, that's why task or start after to, or ends till from
-                        continue;
-                    }
-                    if(!tt.isRepeated() && tt.getTime().after(fromDate) && !tt.getTime().after(toDate)){
-                        // one single active task, to be performed within given range from to
-                        returnedTaskArray.add(tt);
-                    }
-                    else if (tt.isRepeated()){
-                        if(( tt.getStartTime().after(fromDate) && !tt.getStartTime().after(toDate))){
-                            // task fits boundary conditions
-                            returnedTaskArray.add(tt);
-                        }
-                        else if (!tt.getStartTime().after(fromDate)) {
-                            long currtime = tt.getStartTime().getTime();
-                            while (currtime <= from) currtime += tt.getRepeatInterval();
-                            if (currtime <= to) {
-                                // active task, scheduled several times to perform, started already before from
-                                // and should be performed till to
-                                returnedTaskArray.add(tt);
-                            }
-                        }
-                    }
-                } // for( ...
-            }  // if(size() > 0 ...
-        } catch ( IllegalArgumentException ex ){
-            System.err.println(ex.getMessage());
-            ex.printStackTrace();
-        } finally {
-            return returnedTaskArray;
-        }
-    } // incoming(int , int )
-
-
 } // class TaskList
