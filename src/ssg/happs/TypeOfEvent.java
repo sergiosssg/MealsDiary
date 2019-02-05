411be446f14bf5845aea67c484e2fd441470b39e
sergiosssg <sergiosssg@gmail.com>
2019-02-05 17:51:12 +0200

    Added new interfaces :    Event,   Class Schedule transformed into Interface Schedule   Added new enum structures :   TypeOfEvent, TypeOfSchedule
      added block to function MeasureValue.converto

---
+++ src/ssg/happs/TypeOfEvent.java
@@ -0,0 +1,18 @@
+package ssg.happs;
+
+/**
+ * Created by User on 05.02.2019.
+ */
+public enum TypeOfEvent {
+    DREAM,
+    MEAL,
+    HIKING,
+    JOGGING,
+    FITNES,
+    SITTING,
+    LAYING,
+    TVWATCHING,
+    NEARCOMPUTERSITTING,
+    HARDWORKING,
+    OFFICEWORKING
+}
