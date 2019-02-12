6b4c55d4a52dd6eba2641fc1dc4e221cca2b0c28
sergiosssg <sergiosssg@gmail.com>
2019-02-12 11:36:08 +0200

    Created package 'events' and moved into it interface 'EventModel'

--- src/ssg/happs/eventes/EventModel.java
+++ src/ssg/happs/eventes/EventModel.java
@@ -1,11 +1,17 @@
-package ssg.happs;
+package ssg.happs.eventes;
+
+import ssg.happs.AbonentModel;
+import ssg.happs.ObjectRoot;
+import ssg.happs.TypeOfEvent;
 
 /**
  * Created by User on 05.02.2019.
  */
-public interface EventModel extends ObjectRoot{
+public interface EventModel extends ObjectRoot {
 
     long getBeginTime();
+
+    long getDuration();
 
     String getEventName();
 
